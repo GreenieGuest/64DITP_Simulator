c17521112bb7ba7a420e713e972ec7a06847c44d
GreenieGuest <papootreasure@yahoo.com>
2025-07-29 18:04:37 -0500

    added challenges, stats, support for preset profiles

--- 64DITP.py
+++ 64DITP.py
@@ -1,11 +1,13 @@
 import random
+import math
 from time import sleep
 from colorama import Fore, Style
+import json
 
 # [[ CONFIGURATION ]] ----------------------------------------------------------------------------------
 # Player Names
-season_name = "64 Days in the Pit - The Ewowltimate Showdown"
-names = ["losered","TieTiePerson","scorb","#1 sigma Rizz lord","4DJumpman256","X_Ry","Dark","J_duude","Shorky","MayUnderFlowers","Purplegaze","Sparrowcat","JujuMas","GreenieGuest","iRDM","Xyloba","CloudySkyes","RainbowKnight","Brandy?","ThePinkBunnyEmpire","ALITL","Snoozingnewt","SinisterShovel","Midnight Light","whitecyclosa","terminatedslime","Catworld","xXBombs_AwayXx","ArnoobExtra","recc","Grammar Lee","Nerdy Gal","Koopa472","IceKeyHammer","Cohaki","Water Chestnut","LemonVenom","PoliteCheese1414","DogeBone3","PlasmicTrojan","hydrogencitrus","Srimochi","Gizmote","tr_","Deckardv","Juhmatok","mazuat","goobrey","Himo","Paintspot Infez","IntersectingPlanes","Yume Flamigiri","TrainGoBoom","MineFlex_B","#zackd","Twiggy","sictoabu","Swift Smartypants","cloverpepsi","thanos whale.","Onchú","kuminda.water.supply","Indigo","PengiQuin"]
+season_name = "64 Days in the Pit - The Ultimate Showdown"
+names = ["Death Man","Boxy","Polandball","Satan","Stunt Devil","Lukey","Koda","Mitochondria","Pencil","Ronny the Banana","Tom Brady","Atom","Chad","King Lorenzo","Sticky Joe","Trogdor the Burninator","Boat Crew","Terezi Pyrope","Homestar Runner","Carl Grimes","Mr Wilson","Gamer","Hexbug","Four","ChrisPop","Muscles","Erika Faust","Ellie Lander","Teresa Tayliss","Dr Horrible","Billy Blockhead","Nagito Komaeda","Joe","Boston Rob","Dog","Tommy Walter","Germanyball","Americaball","Big Rig","Transparent Red","Joey","Petra","Isaac Creighton","Hunter Moses","Jack Dawson","Josephine Mercier","Roco","Horatio Caine","Frederick","Watch","V2","Nitro","Vriska Serket","Todd","Jackson","2-D","OG","Mr Ben","Tom","Lauren","Martin","Captain Hammer","Heathcliff","The Miser"]
 # First Quarter Tribe Names
 q1Names = ["Tomamasi","Raging Rapids","Dangerous Dynamites","Eclipsed Survivors"]
 q1Colors = [Fore.YELLOW, Fore.BLUE, Fore.RED, Fore.GREEN]
@@ -23,6 +25,11 @@ mergeColor = Fore.YELLOW
 juryName = "Jury"
 # Invincibility Name
 immunityName = "Individual Immunity"
+
+FASTFORWARD = True
+
+PRESET_PROFILES = False
+PROFILE_FILE_PATH = 'profiles.json'
 
 # If you know what you're doing, have fun tweaking below! ----------------------------------------------------------------------------------
 
@@ -52,7 +59,163 @@ class Player:
         self.notoriety = notoriety
         self.faction = faction
 
+def decode(obj):
+    return Player(obj['name'], 0, 0, None, None, None, obj['physStat'], obj['stratStat'], obj['socStat'], obj['notoriety'], obj['faction'])
+    
 # [[ SIM FUNCTIONS ]] ----------------------------------------------------------------------------------
+
+def teamChallenge():
+    challengeTypes = ["physical", "mental", "teamwork", "puzzle", "luck"]
+    challenge = random.choice(challengeTypes)
+    challenges.append(challenge)
+
+    teamSize = teams.copy()
+    teamSize.sort(key=len)
+
+    participating = len(teamSize[0]) # Sit out extra members to make things fair
+
+    teamPoints = [0] * len(teams)
+    loserId = None
+    match challenge:
+        case "physical":
+            # Generic Challenge Format
+
+            print("Challenge: Physical")
+            for team in range(0, len(teams)):
+                points = 0
+                for player in range(participating):
+                    playerRoll = random.randint(1, teams[team][player].physStat)
+
+                    points += playerRoll
+                
+                teamPoints[team] = points
+        case "mental":
+            # Generic Challenge Format
+
+            print("Challenge: Mental")
+            for team in range(0, len(teams)):
+                points = 0
+                for player in range(participating):
+                    playerRoll = random.randint(1, teams[team][player].stratStat)
+
+                    points += playerRoll
+
+                teamPoints[team] = points
+        case "teamwork":
+            # Generic Challenge Format
+
+            print("Challenge: Teamwork")
+            for team in range(0, len(teams)):
+                points = 0
+                for player in range(participating):
+                    playerRoll = random.randint(1, teams[team][player].socStat)
+
+                    points += playerRoll
+
+                teamPoints[team] = points
+        case "puzzle":
+            print("Challenge: Puzzle")
+            for team in range(0, len(teams)):
+                points = 0
+                for player in range(participating):
+                    playerRoll1 = random.randint(1, teams[team][player].socStat)
+                    playerRoll2 = random.randint(1, teams[team][player].stratStat)
+
+                    points += (playerRoll1 * playerRoll2)
+
+                teamPoints[team] = points
+        case _:
+            print("Challenge: Luck")
+            for team in range(0, len(teams)):
+                points = random.randint(1, 20)
+
+                teamPoints[team] = points
+
+    # Sort the points per team, then find the team with lowest points and declare them the loser.
+    # Functionality for multiple losers later
+
+    results = teamPoints.copy()
+    results.sort()
+    
+    print(f"Results:")
+    for x in range(len(teams)):
+        print(f"{teamColors[x]}{teamNames[x]}{Style.RESET_ALL}: {teamPoints[x]}")
+    loserId = teamPoints.index(results[0]) # First element in results is losing team
+    return loserId
+
+def challengeMerge(ultimateShowdown, showdownRound):
+    if ultimateShowdown == True:
+        challenge = challenges[showdownRound]
+    else:
+        challengeTypes = ["physical", "mental", "elimination"]
+        challenge = random.choice(challengeTypes)
+        challenges.append(challenge)
+
+    playerPoints = [0] * len(notChosen)
+    match challenge:
+        case "physical":
+            # Generic Challenge Format
+
+            print("Challenge: Physical")
+            for player in range(len(notChosen)):
+                points = random.randint(1, notChosen[player].physStat)
+                
+                playerPoints[player] = points
+        case "mental":
+            print("Challenge: Mental")
+            for player in range(len(notChosen)):
+                points = random.randint(1, notChosen[player].stratStat)
+                
+                playerPoints[player] = points
+        case "elimination":
+            print("Challenge: Elimination")
+            for player in range(len(notChosen)):
+                points = random.randint(1, notChosen[player].socStat)
+                
+                playerPoints[player] = points
+
+        # Pre-Merge Challenges (for Ultimate Showdown)
+
+        case "teamwork":
+            print("Challenge: Teamwork")
+            for player in range(len(notChosen)):
+                points = random.randint(1, notChosen[player].socStat)
+                
+                playerPoints[player] = points
+        case "puzzle":
+            print("Challenge: Puzzle")
+            for player in range(len(notChosen)):
+                playerRoll1 = random.randint(1, notChosen[player].socStat)
+                playerRoll2 = random.randint(1, notChosen[player].stratStat)
+
+                points = (playerRoll1 * playerRoll2)
+                
+                playerPoints[player] = points
+        case _:
+            print("Challenge: Luck")
+            for player in range(len(notChosen)):
+                points = random.randint(1, 20)
+
+                playerPoints[player] = points
+
+    # Sort the points per player, returning a list of players from MOST POINTS to LEAST.
+
+    results = playerPoints.copy()
+    results.sort(reverse=True)
+    
+    print(f"Results:")
+    for x in range(len(notChosen)):
+        print(f"{notChosen[x].name}: {playerPoints[x]}")
+    for x in range(len(results)):
+
+        index = playerPoints.index(results[x])
+        results[x] = index
+        playerPoints[index] = 0 # Clears the player's index so that repeat values aren't used. This breaks the sim
+        
+    
+    wait(1)
+
+    return results
 
 def Elimination(Team):
 
@@ -61,13 +224,15 @@ def Elimination(Team):
     eliminated = random.choice(Team) # For now (and as in 2018) the eliminated contestant is random
 
     print(f"{65 - numPlayers}{suffix(65 - numPlayers)} person voted out of {season_name}...")
-    sleep(3)
+    wait(3)
     print(f"{eliminated.name}. {numPlayers - 1} remain.\n")
     Team.remove(eliminated)
 
     if numPlayers < 17:
         print(f"They are inducted as the {17 - numPlayers}{suffix(17 - numPlayers)} member of the {juryName}.\n")
         jury.append(eliminated)
+
+    bootOrder.insert(0, eliminated)
 
 def printTeams(showTeams):
     for x in range(len(teams)):
@@ -114,6 +279,13 @@ def proceed():
     proceed = input("Press enter to proceed.")
     print(" ")
 
+def getShowdownPoints(player):
+    """
+    Get a stat.
+    """
+    return player.showdownPoints
+
+
 def rollPass(stat):
     """
     The basic scheme for randomizing performance based on a stat.
@@ -131,26 +303,67 @@ def clearTeams():
         notChosen.extend(x)
     teams.clear()
 
+def wait(time): # Lua relic
+    if FASTFORWARD == False:
+        sleep(time)
+
 # [[ SIMULATION ]] ----------------------------------------------------------------------------------
 
 players = [Player(item, 0, 0, None, None, None, random.randint(1, 6), random.randint(1, 6), random.randint(1, 6), 0, "Unaffiliated") for item in names]
+
+if PRESET_PROFILES == True:
+    try:
+        with open(PROFILE_FILE_PATH, 'r') as f:
+            data = json.load(f, object_hook=decode)
+
+            for player in range(len(data)): # If not 64 players, then replace up to that many players
+                players[player] = data[player]
+    except FileNotFoundError:
+        print(f"Error: file '{PROFILE_FILE_PATH}' not found")
+    except json.JSONDecodeError:
+        print(f"Error: syntax error in '{PROFILE_FILE_PATH}'")
+    except Exception as e:
+        print(f"Error: {e}")
+proceed()
+
 notChosen = players.copy()
 
 teams = [[],[],[],[]]
 teamNames = q1Names
 teamColors = q1Colors
 
+teamCaptains = [None, None, None, None] # Purely cosmetic
+
 print(f"{season_name}")
 print(f"{Fore.GREEN}[[DAY ONE SCHOOLYARD PICK]]{Style.RESET_ALL}")
 teamId = 0
+
+for x in range(4):
+    teamCaptain = random.choice(notChosen)
+    teams[teamId].append(teamCaptain)
+    teamCaptain.color1 = teamColors[teamId]
+    teamCaptains[teamId] = teamCaptain
+    notChosen.remove(teamCaptain)
+    teamId += 1
+    if teamId > 3:
+        teamId = 0
+
+print(f"The team captains are randomly selected as {teamCaptains[0].name}, {teamCaptains[1].name}, {teamCaptains[2].name}, and {teamCaptains[3].name}.")
+wait(1)
+
 while len(notChosen) > 0:
     player = random.choice(notChosen)
+
+    # Future logic for choosing teams here
+
+    print(f"{teamCaptains[teamId].name} chooses {player.name}.")
     teams[teamId].append(player)
     player.color1 = teamColors[teamId]
     notChosen.remove(player)
     teamId += 1
     if teamId > 3:
         teamId = 0
+    wait(0.5)
 
 printTeams(True)
 print(f"{Fore.GREEN}[[FIRST QUARTER - FOUR-TEAM PHASE]]{Style.RESET_ALL}")
@@ -158,11 +371,12 @@ proceed()
 
 while numPlayers > 48:
     print(f"[- Day {65 - numPlayers} -]\n")
-    losers = random.randint(0, 3)
-    print(f"{teamNames[losers]} lost the challenge!")
+    losers = teamChallenge()
 
+    print(f"\n{teamNames[losers]} lost the challenge!")
+    wait(1)
     printPlayersIn(teams[losers], teamNames[losers], teamColors[losers], False)
-    sleep(2)
+    wait(2)
     Elimination(teams[losers])
     numPlayers -= 1
 
@@ -193,11 +407,12 @@ proceed()
 
 while numPlayers > 32:
     print(f"[- Day {65 - numPlayers} -]\n")
-    losers = random.randint(0, 2)
-    print(f"{teamNames[losers]} lost the challenge!")
+    losers = teamChallenge()
+    print(f"\n{teamNames[losers]} lost the challenge!")
+    wait(1)
 
     printPlayersIn(teams[losers], teamNames[losers], teamColors[losers], False)
-    sleep(2)
+    wait(2)
     Elimination(teams[losers])
     numPlayers -= 1
 
@@ -229,11 +444,12 @@ proceed()
 
 while numPlayers > 16:
     print(f"[- Day {65 - numPlayers} -]\n")
-    losers = random.randint(0, 1)
-    print(f"{teamNames[losers]} lost the challenge!")
+    losers = teamChallenge()
+    print(f"\n{teamNames[losers]} lost the challenge!")
+    wait(1)
 
     printPlayersIn(teams[losers], teamNames[losers], teamColors[losers], False)
-    sleep(2)
+    wait(2)
     Elimination(teams[losers])
     numPlayers -= 1
 
@@ -255,15 +471,24 @@ while numPlayers > 8:
     teamNames = ["Immune","Lost the Challenge"]
     teamColors = q4Colors
     teamId = 0
-    while len(notChosen) > 0:   
-        player = random.choice(notChosen)
-        teams[teamId].append(player)
-        notChosen.remove(player)
-        teamId += 1
-        if teamId > 1:
-           teamId = 0
+
+    challengeResults = challengeMerge(False, 0)
+    threshold = math.ceil(numPlayers / 2)
+    dangerzone = notChosen.copy()
+
+    for x in range(threshold): # In first half of merge, number of immune contestants is always favored in odd rounds
+        player = notChosen[challengeResults[x]] # Challenge results returns the indexes in order from best challenge performance to worst
+        player.notoriety += 1
+
+        teams[0].append(player) 
+        dangerzone.remove(player)
+    # Dump the losers in the loser team
+    teams[1] = dangerzone.copy()
+    del dangerzone
+    notChosen.clear()
+
     printTeams(False)
-    sleep(2)
+    wait(2)
 
     Elimination(teams[1])
     numPlayers -= 1
@@ -278,11 +503,15 @@ proceed()
 while numPlayers > 3:
     print(f"[- Day {65 - numPlayers} -]\n")
     printPlayersIn(notChosen, mergeName, mergeColor, False)
-    immune = random.choice(notChosen)
+    
+    challengeResults = challengeMerge(False, 0)
+    immune = notChosen[challengeResults[0]] # In Elite 8 phase, only the top challenge performer is given immunity
+    immune.notoriety += 1
+
     notChosen.remove(immune)
     print(f"{immune.name} won {immunityName}.")
     
-    sleep(2)
+    wait(2)
 
     Elimination(notChosen)
     numPlayers -= 1
@@ -298,27 +527,32 @@ proceed()
 # Ultimate Showdown
 
 for x in range(61):
-    challengeWinner = random.choice(notChosen)
+    challengeResults = challengeMerge(True, x)
+    challengeWinner = notChosen[challengeResults[0]]
     challengeWinner.showdownPoints += 1
 
-    print(f"Round {x+1}: {challengeWinner.name} wins. | {notChosen[0].name}: {notChosen[0].showdownPoints}, {notChosen[1].name}: {notChosen[1].showdownPoints}, {notChosen[2].name}: {notChosen[2].showdownPoints}")
-    sleep(0.5)
+    print(f"Round {x+1}: {challengeWinner.name} wins. | {notChosen[0].name}: {notChosen[0].showdownPoints}, {notChosen[1].name}: {notChosen[1].showdownPoints}, {notChosen[2].name}: {notChosen[2].showdownPoints}\n")
+    wait(0.5)
 
 fallenAngel = None
-if notChosen[0].showdownPoints < notChosen[1].showdownPoints and notChosen[0].showdownPoints < notChosen[2].showdownPoints:
-    fallenAngel = notChosen[0]
-elif notChosen[1].showdownPoints < notChosen[0].showdownPoints and notChosen[1].showdownPoints < notChosen[2].showdownPoints:
-    fallenAngel = notChosen[1]
-elif notChosen[2].showdownPoints < notChosen[0].showdownPoints and notChosen[2].showdownPoints < notChosen[1].showdownPoints:
-    fallenAngel = notChosen[2]
+notChosen.sort(reverse=True, key=getShowdownPoints)
+if notChosen[2].showdownPoints == notChosen[1].showdownPoints:
+    # Two-Way Tie, leader decides. (Three-Way Tie is possible with 60 but not with 61.)
+    print(f"{notChosen[1].name} and {notChosen[2].name} tied. {notChosen[0].name}, the frontrunner, will cast the final vote for elimination.")
+    fallenAngel = random.choice([notChosen[1], notChosen[2]])
+    wait(1)
+    print(f"They vote for {fallenAngel.name}.")
 else: #Tie
-    fallenAngel = random.choice(notChosen) # replace with tiebreaker later
+    fallenAngel = notChosen[2]
+
+wait(1)
 
 notChosen.remove(fallenAngel)
 print(f"{fallenAngel.name} failed to qualify for the finale after the Ultimate Showdown, and was eliminated in 3rd place.")
 print(f"They are inducted as the {17 - numPlayers}th and final member of the {juryName}.\n")
 numPlayers -= 1
 jury.append(fallenAngel)
+bootOrder.insert(0, fallenAngel)
 
 printPlayersIn(notChosen, mergeName, mergeColor, True)
 print(f"[- Day 64 -]\n")
@@ -327,11 +561,17 @@ print(f"{Fore.GREEN}[[FINAL TWO - JURY VOTE]]{Style.RESET_ALL}")
 proceed()
 
 for x in range(len(jury)):
-    votePick = random.choice(notChosen)
+    weight = random.randint(0, notChosen[0].notoriety + notChosen[1].notoriety) # Chances of one player being voted for is based on how much they've played the game by winning challenges, etc
+    if weight < notChosen[0].notoriety:
+        votePick = notChosen[0]
+    else:
+        votePick = notChosen[1]
     votePick.juryVotes += 1
 
+    # Bitter juries don't exist in 64DITP
+
     print(f"{jury[x].name} votes for {votePick.name}.\nThat's {notChosen[0].juryVotes} votes {notChosen[0].name}, {notChosen[1].juryVotes} votes {notChosen[1].name}, {len(jury) - (x + 1)} votes left.")
-    sleep(1)
+    wait(1)
 
 runnerUp = None
 if notChosen[0].juryVotes < notChosen[1].juryVotes:
@@ -340,8 +580,12 @@ elif notChosen[1].juryVotes < notChosen[0].juryVotes:
     runnerUp = notChosen[1]
 else: #Tie
     print(f"The votes tied. {fallenAngel.name}, the final {juryName} member, will cast an additional final vote.")
-    sleep(1)
-    votePick = random.choice(notChosen)
+    wait(1)
+    weight = random.randint(notChosen[0].notoriety + notChosen[1].notoriety) # Chances of one player being voted for is based on how much they've played the game by winning challenges, etc
+    if weight < notChosen[0].notoriety:
+        votePick = notChosen[0]
+    else:
+        votePick = notChosen[1]
     votePick.juryVotes += 1
     print(f"They vote for {votePick.name}.")
 
@@ -350,12 +594,23 @@ else: #Tie
     else:
         runnerUp = notChosen[1]
 
+bootOrder.insert(0, runnerUp)
 notChosen.remove(runnerUp)
 print(f"{runnerUp.name} failed to win the {juryName} Vote, and was eliminated in 2nd place as the runner-up.\n")
 numPlayers -= 1
 
 winner = random.choice(notChosen)
+bootOrder.insert(0, winner)
 notChosen.remove(winner)
 print(f"{winner.name} is the winner of {season_name}.")
 
-sleep(10)
+proceed()
+for x in range(len(bootOrder)):
+    if x < 16:
+        print(f"{x+1}{suffix(x+1)}: {bootOrder[x].name}" + f" [{bootOrder[x].color1}*{bootOrder[x].color2}*{bootOrder[x].color3}*{Style.RESET_ALL}]")
+    elif x < 32:
+        print(f"{x+1}{suffix(x+1)}: {bootOrder[x].color3}{bootOrder[x].name}{Style.RESET_ALL}" + f" [{bootOrder[x].color1}*{bootOrder[x].color2}*{Style.RESET_ALL}]")
+    elif x < 48:
+        print(f"{x+1}{suffix(x+1)}: {bootOrder[x].color2}{bootOrder[x].name}{Style.RESET_ALL}" + f" [{bootOrder[x].color1}*{Style.RESET_ALL}]")
+    else:
+        print(f"{x+1}{suffix(x+1)}: {bootOrder[x].color1}{bootOrder[x].name}{Style.RESET_ALL}")
