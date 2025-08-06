5277c70ee5a4575a5bcc1143d94f0bf1d2b7188b
GreenieGuest <papootreasure@yahoo.com>
2025-08-05 19:51:57 -0500

    Update and rename 64DITP.py to src/main.py

---
+++ src/main.py
@@ -0,0 +1,439 @@
+from colorama import Fore, Style
+from pathlib import Path
+import random
+import math
+import json
+import os
+
+from challenges import teamChallenge, challengeMerge
+from gamefunctions import gameEvents, schoolyardPick, elimination
+from utils import suffix, getShowdownPoints, proceed, wait
+from config import season_name, names, q1Names, q1Colors, q2Names, q2Colors, q3Names, q3Colors, mergeName, q4Colors, mergeColor, juryName, immunityName, PRESET_PROFILES, PROFILE_FILE_PATH
+
+file = Path(__file__)
+parent = file.parent
+os.chdir(parent)
+
+# If you know what you're doing, have fun tweaking below! ----------------------------------------------------------------------------------
+
+# [[ IMPORTANT SIMULATION STUFF ]] ----------------------------------------------------------------------------------
+castSize = len(names)
+numPlayers = len(names)
+jury = []
+bootOrder = []
+voteNotations = []
+challenges = []
+quarter = 1
+
+class Player:
+    def __init__(self, name, physStat, stratStat, socStat, notoriety, faction):
+        self.name = name
+        # Game Points
+        self.showdownPoints = 0
+        self.juryVotes = 0
+
+        self.idols = []
+
+        # Team Colors
+        self.color1 = None
+        self.color2 = None
+        self.color3 = None
+
+        # Stats
+        self.physStat = physStat
+        self.stratStat = stratStat
+        self.socStat = socStat
+        self.notoriety = notoriety
+        self.faction = faction
+
+#class Faction: #Unused
+#    def __init__(self, name, color, founder):
+#        self.name = name
+#        self.color = color
+#        self.founder = founder
+#        self.members = [founder]
+#        
+#    def add_member(self, player):
+#        self.members.append(player)
+#        player.faction = self
+#
+#    def disband(self):
+#        for member in self.members:
+#           member.faction = "Unaffiliated"
+#        self.members.clear()
+
+def decode(obj):
+    return Player(obj['name'], obj['physStat'], obj['stratStat'], obj['socStat'], obj['notoriety'], obj['faction'])
+
+def logBootOrder():
+    with open("simulations.txt", "w") as log:
+        for x in range(len(bootOrder)):
+            log.write(f"{bootOrder[x].name}\n") # {x+1}{suffix(x+1)}: 
+        log.write("\n")
+
+    print("Simulation boot order logged in simulations.txt")
+    
+# [[ SIM FUNCTIONS ]] ----------------------------------------------------------------------------------
+
+def printTeams(showTeams):
+    for x in range(len(teams)):
+        print(f"{teamColors[x]}[[{teamNames[x]}]]{Style.RESET_ALL}")
+        for z in teams[x]:
+            if showTeams == True:
+                match quarter:
+                    case 2:
+                        print(z.name + f" [{z.color1}*{Style.RESET_ALL}]")
+                    case 3:
+                        print(z.name + f" [{z.color1}*{z.color2}*{Style.RESET_ALL}]")
+                    case 4:
+                        print(z.name + f" [{z.color1}*{z.color2}*{z.color3}*{Style.RESET_ALL}]")
+                    case _:
+                        print(z.name)
+            else:
+                if z.faction == "Unaffiliated":
+                    print(z.name + f" [{z.notoriety}]")
+                else:
+                    print(z.name + f" [{z.notoriety}] ({z.faction})")
+        print(" ")
+        
+def printPlayersIn(Team, name, color, showTeams):
+    print(f"{color}[[{name}]]{Style.RESET_ALL}")
+    for x in Team:
+        if showTeams == True:
+            match quarter:
+                case 2:
+                    print(x.name + f" [{x.color1}*{Style.RESET_ALL}]")
+                case 3:
+                    print(x.name + f" [{x.color1}*{x.color2}*{Style.RESET_ALL}]")
+                case 4:
+                    print(x.name + f" [{x.color1}*{x.color2}*{x.color3}*{Style.RESET_ALL}]")
+                case _:
+                    print(x.name)
+        else:
+            if x.faction == "Unaffiliated":
+                print(x.name + f" [{x.notoriety}]")
+            else:
+                print(x.name + f" [{x.notoriety}] ({x.faction})")
+    print(" ")
+
+def clearTeams():
+    for x in teams:
+        notChosen.extend(x)
+    teams.clear()
+
+def mergeElimination(originalNominated, originalVotingPool):
+    eliminated, votingNotation = elimination(originalNominated, originalVotingPool)
+
+    print(f"{castSize + 1 - numPlayers}{suffix(castSize + 1 - numPlayers)} person voted out of {season_name}...")
+    wait(3)
+    print(f"{eliminated.name}. {numPlayers - 1} remain.\n")
+    print(f"Vote Notation: {votingNotation}")
+    voteNotations.append(votingNotation)
+    
+    originalNominated.remove(eliminated)
+
+    if numPlayers < 17:
+        print(f"They are inducted as the {17 - numPlayers}{suffix(17 - numPlayers)} member of the {juryName}.\n")
+        jury.append(eliminated)
+
+    players.remove(eliminated)
+    bootOrder.insert(0, eliminated)
+
+def teamRound():
+    global numPlayers
+    print(f"[- Day {castSize + 1 - numPlayers} -]\n")
+    for i, team in enumerate(teams):
+        print(f"{teamColors[i]}[[{teamNames[i]} Events]]{Style.RESET_ALL}")
+        gameEvents(team, players)
+    wait(1)
+    losers = teamChallenge(challenges, teams, teamNames, teamColors)
+    print(f"\n{teamNames[losers]} lost the challenge!")
+    wait(1)
+    for i, team in enumerate(teams):
+        print(f"{teamColors[i]}[[{teamNames[i]} Events]]{Style.RESET_ALL}")
+        gameEvents(team, players)
+    wait(1)
+
+    printPlayersIn(teams[losers], teamNames[losers], teamColors[losers], False)
+    wait(2)
+    eliminated, votingNotation = elimination(teams[losers], teams[losers])
+
+    print(f"{castSize + 1 - numPlayers}{suffix(castSize + 1 - numPlayers)} person voted out of {season_name}...")
+    wait(3)
+    print(f"{eliminated.name}. {numPlayers - 1} remain.\n")
+    print(f"Vote Notation: {votingNotation}")
+    voteNotations.append(votingNotation)
+    
+    teams[losers].remove(eliminated)
+
+    players.remove(eliminated)
+    bootOrder.insert(0, eliminated)
+    numPlayers -= 1
+    
+    proceed()
+
+def teamSwap(numTeams):
+    teamId = 0
+    while len(notChosen) > 0:
+        player = random.choice(notChosen)
+        teams[teamId].append(player)
+        match numTeams:
+            case 3:
+                player.color2 = teamColors[teamId]
+            case 2:
+                player.color3 = teamColors[teamId]
+        notChosen.remove(player)
+        teamId += 1
+        if teamId > numTeams - 1:
+            teamId = 0
+
+# [[ SIMULATION ]] ----------------------------------------------------------------------------------
+
+players = [Player(item, random.randint(1, 6), random.randint(1, 6), random.randint(1, 6), 0, "Unaffiliated") for item in names]
+
+if PRESET_PROFILES == True:
+    try:
+        with open(PROFILE_FILE_PATH, 'r') as f:
+            data = json.load(f, object_hook=decode)
+
+            for player in range(min(len(players), len(data))): # If not 64 players, then replace up to that many players
+                players[player] = data[player]
+
+            # if more than default entries, add them to the pile so they might be picked out via random sample
+            if len(data) > len(players):
+                players.extend(data[len(players):])
+    except FileNotFoundError:
+        print(f"Error: file '{PROFILE_FILE_PATH}' not found")
+    except json.JSONDecodeError:
+        print(f"Error: syntax error in '{PROFILE_FILE_PATH}'")
+    except Exception as e:
+        print(f"Error: {e}")
+proceed()
+
+notChosen = players.copy()
+
+teams = [[],[],[],[]]
+teamNames = q1Names
+teamColors = q1Colors
+
+print(f"{season_name}")
+schoolyardPick(notChosen, teams, teamColors)
+
+printTeams(True)
+print(f"{Fore.GREEN}[[FIRST QUARTER - FOUR-TEAM PHASE]]{Style.RESET_ALL}")
+proceed()
+
+while numPlayers > 48:
+    teamRound()
+printTeams(True)
+print(f"{Fore.GREEN}[[SECOND QUARTER - THREE-TEAM PHASE]]{Style.RESET_ALL}")
+quarter += 1
+proceed()
+# First Quarter End
+clearTeams()
+
+teams = [[],[],[]]
+teamNames = q2Names
+teamColors = q2Colors
+teamSwap(3)
+
+printTeams(True)
+print("The teams have been swapped randomly. The Second Quarter begins.")
+proceed()
+
+while numPlayers > 32:
+    teamRound()
+printTeams(True)
+print(f"{Fore.GREEN}[[THIRD QUARTER - TWO-TEAM PHASE]]{Style.RESET_ALL}")
+quarter += 1
+proceed()
+
+# Second Quarter End
+clearTeams()
+
+teams = [[],[]]
+teamNames = q3Names
+teamColors = q3Colors
+teamSwap(2)
+
+printTeams(True)
+print("The halfway point of players has been reached. The teams have been swapped again. The Third Quarter begins.")
+proceed()
+
+while numPlayers > 16:
+    teamRound()
+printTeams(False)
+proceed()
+# Third Quarter End
+quarter += 1
+print("The teams have officially merged. For the first half of the merge, the first half of competitors who win the challenge win immunity.")
+clearTeams()
+printPlayersIn(notChosen, mergeName, mergeColor, True)
+print(f"{Fore.GREEN}[[FOURTH QUARTER - MERGE (HAVE-GOTS vs. HAVE-NOTS)]]{Style.RESET_ALL}")
+proceed()
+
+while numPlayers > 8:
+    print(f"[- Day {castSize + 1 - numPlayers} -]\n")
+
+    gameEvents(players, players)
+
+    teams = [[],[]]
+    teamNames = ["Immune","Lost the Challenge"]
+    teamColors = q4Colors
+    teamId = 0
+
+    challengeResults = challengeMerge(False, 0, challenges, notChosen)
+    threshold = math.ceil(numPlayers / 2)
+    dangerzone = notChosen.copy()
+
+    for x in range(threshold): # In first half of merge, number of immune contestants is always favored in odd rounds
+        player = notChosen[challengeResults[x]] # Challenge results returns the indexes in order from best challenge performance to worst
+        player.notoriety += 1
+
+        teams[0].append(player) 
+        dangerzone.remove(player)
+    # Dump the losers in the loser team
+    teams[1] = dangerzone.copy()
+    del dangerzone
+    notChosen.clear()
+
+    gameEvents(players, players)
+    wait(2)
+    printTeams(False)
+
+    mergeElimination(teams[1], teams[0]+teams[1])
+    numPlayers -= 1
+    clearTeams()
+
+    proceed()
+print("The Have-Gots vs. Have-Nots phase is over. Individual immunity is now in effect.")
+printPlayersIn(notChosen, mergeName, mergeColor, True)
+print(f"{Fore.GREEN}[[FOURTH QUARTER - MERGE (INDIVIDUAL IMMUNITY)]]{Style.RESET_ALL}")
+proceed()
+
+while numPlayers > 3:
+    print(f"[- Day {castSize + 1 - numPlayers} -]\n")
+    printPlayersIn(notChosen, mergeName, mergeColor, False)
+    gameEvents(players, players)
+    wait(2)
+    
+    challengeResults = challengeMerge(False, 0, challenges, notChosen)
+    immune = notChosen[challengeResults[0]] # In Elite 8 phase, only the top challenge performer is given immunity
+    immune.notoriety += 1
+
+    notChosen.remove(immune)
+    print(f"{immune.name} won {immunityName}.")
+    
+    wait(2)
+    gameEvents(players, players)
+    wait(2)
+
+    mergeElimination(notChosen, notChosen+[immune])
+    numPlayers -= 1
+    notChosen.append(immune)
+
+    proceed()
+printPlayersIn(notChosen, mergeName, mergeColor, True)
+print(f"[- Day {castSize - 2} -]\n")
+print("The final challenge, to decide which of the final 3 makes it to the final 2, is a marathon of every previous challenge in order. The player with the least wins after all 61 rounds will fail to qualify.")
+print(f"{Fore.GREEN}[[FINAL THREE CHALLENGE - THE ULTIMATE SHOWDOWN]]{Style.RESET_ALL}")
+proceed()
+
+# Ultimate Showdown
+
+for x in range(61):
+    challengeResults = challengeMerge(True, x, challenges, notChosen)
+    challengeWinner = notChosen[challengeResults[0]]
+    challengeWinner.showdownPoints += 1
+
+    print(f"Round {x+1}: {challengeWinner.name} wins. | {notChosen[0].name}: {notChosen[0].showdownPoints}, {notChosen[1].name}: {notChosen[1].showdownPoints}, {notChosen[2].name}: {notChosen[2].showdownPoints}\n")
+    wait(0.5)
+
+fallenAngel = None
+notChosen.sort(reverse=True, key=getShowdownPoints)
+if notChosen[2].showdownPoints == notChosen[1].showdownPoints:
+    # Two-Way Tie, leader decides. (Three-Way Tie is possible with 60 but not with 61.)
+    print(f"{notChosen[1].name} and {notChosen[2].name} tied. {notChosen[0].name}, the frontrunner, will cast the final vote for elimination.")
+    fallenAngel = random.choice([notChosen[1], notChosen[2]])
+    wait(1)
+    print(f"They vote for {fallenAngel.name}.")
+else: #Tie
+    fallenAngel = notChosen[2]
+
+wait(1)
+
+voteNotations.append(f"{notChosen[0].showdownPoints}-{notChosen[1].showdownPoints}-{notChosen[2].showdownPoints}")
+notChosen.remove(fallenAngel)
+print(f"{fallenAngel.name} failed to qualify for the finale after the Ultimate Showdown, and was eliminated in 3rd place.")
+print(f"They are inducted as the {17 - numPlayers}th and final member of the {juryName}.\n")
+numPlayers -= 1
+jury.append(fallenAngel)
+bootOrder.insert(0, fallenAngel)
+
+printPlayersIn(notChosen, mergeName, mergeColor, True)
+print(f"[- Day {castSize} -]\n")
+print("The members of the jury, who all were eliminated after the merge, will now decide the winner.")
+print(f"{Fore.GREEN}[[FINAL TWO - JURY VOTE]]{Style.RESET_ALL}")
+proceed()
+
+for x in range(len(jury)):
+    weight = random.randint(0, notChosen[0].notoriety + notChosen[1].notoriety) # Chances of one player being voted for is based on how much they've played the game by winning challenges, etc
+    if weight < notChosen[0].notoriety:
+        votePick = notChosen[0]
+    else:
+        votePick = notChosen[1]
+    votePick.juryVotes += 1
+
+    # Bitter juries don't exist in 64DITP
+
+    print(f"{jury[x].name} votes for {votePick.name}.\nThat's {notChosen[0].juryVotes} votes {notChosen[0].name}, {notChosen[1].juryVotes} votes {notChosen[1].name}, {len(jury) - (x + 1)} votes left.")
+    wait(1)
+
+runnerUp = None
+if notChosen[0].juryVotes < notChosen[1].juryVotes:
+    runnerUp = notChosen[0]
+elif notChosen[1].juryVotes < notChosen[0].juryVotes:
+    runnerUp = notChosen[1]
+else: #Tie
+    print(f"The votes tied. {fallenAngel.name}, the final {juryName} member, will cast an additional final vote.")
+    wait(1)
+    weight = random.randint(0, notChosen[0].notoriety + notChosen[1].notoriety) # Chances of one player being voted for is based on how much they've played the game by winning challenges, etc
+    if weight < notChosen[0].notoriety:
+        votePick = notChosen[0]
+    else:
+        votePick = notChosen[1]
+    votePick.juryVotes += 1
+    print(f"They vote for {votePick.name}.")
+
+    if notChosen[0].juryVotes < notChosen[1].juryVotes:
+        runnerUp = notChosen[0]
+    else:
+        runnerUp = notChosen[1]
+
+bootOrder.insert(0, runnerUp)
+notChosen.remove(runnerUp)
+print(f"{runnerUp.name} failed to win the {juryName} Vote, and was eliminated in 2nd place as the runner-up.\n")
+numPlayers -= 1
+
+winner = random.choice(notChosen)
+bootOrder.insert(0, winner)
+notChosen.remove(winner)
+print(f"{winner.name} is the winner of {season_name}.")
+
+voteNotations.append(f"{runnerUp.juryVotes}-{winner.juryVotes}")
+voteNotations.append(f"{winner.juryVotes}-{runnerUp.juryVotes}")
+
+proceed()
+for x in range(len(bootOrder)):
+    if x < 16:
+        print(f"{x+1}{suffix(x+1)}: {bootOrder[x].name}" + f" [{bootOrder[x].color1}*{bootOrder[x].color2}*{bootOrder[x].color3}*{Style.RESET_ALL}] - {voteNotations[castSize - x - 1]}")
+    elif x < 32:
+        print(f"{x+1}{suffix(x+1)}: {bootOrder[x].color3}{bootOrder[x].name}{Style.RESET_ALL}" + f" [{bootOrder[x].color1}*{bootOrder[x].color2}*{Style.RESET_ALL}] - {voteNotations[castSize - x - 1]}")
+    elif x < 48:
+        print(f"{x+1}{suffix(x+1)}: {bootOrder[x].color2}{bootOrder[x].name}{Style.RESET_ALL}" + f" [{bootOrder[x].color1}*{Style.RESET_ALL}] - {voteNotations[castSize - x - 1]}")
+    else:
+        print(f"{x+1}{suffix(x+1)}: {bootOrder[x].color1}{bootOrder[x].name}{Style.RESET_ALL} - {voteNotations[castSize - x - 1]}")
+
+logBootOrder()
+        
+proceed()
